13ebaa902655f4d17297e583a9c88d23c617b8c2
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-01-02 19:46:25 -0500

    feat: add hook support
    Resolves #54

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,13 @@
 const { pluginConfigSchema } = require('./schema');
 
 module.exports = {
-	default: () => ({ prefix: '/api/' }),
+	default: () => ({
+		prefix: '/api/',
+		hooks: {
+			preResponseTransform: (ctx) => {},
+			postResponseTransform: (ctx) => {},
+		},
+	}),
 	validator: async (config) => {
 		await pluginConfigSchema.validate(config);
 	},
