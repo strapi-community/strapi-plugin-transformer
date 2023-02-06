80ee75e16206b9761b5802e164f66e9f65cae18e
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-02-06 01:19:45 -0500

    feat(middleware): add denyList support

--- server/register.js
+++ server/register.js
@@ -3,8 +3,11 @@
 const _ = require('lodash');
 
 const { transform } = require('./middleware/transform');
+const { getPluginService } = require('./util/getPluginService');
 
 module.exports = ({ strapi }) => {
+	const settings = getPluginService('settingsService').get();
+
 	// register transforms on all api routes
 	const apis = _.get(strapi, ['api'], {});
 	for (const ct in apis) {
@@ -12,8 +15,22 @@ module.exports = ({ strapi }) => {
 			continue;
 		}
 
+		// skip any uids in denylist that are completely denied
+		const uid = apis[ct].contentTypes[ct].uid;
+		const isDeniedUID = _.get(settings, ['denyList', uid], false);
+		if (isDeniedUID && typeof isDeniedUID === 'boolean') {
+			continue;
+		}
+
 		const apiRoutes = _.get(apis, [ct, 'routes', ct, 'routes'], []);
 		for (let i = 0; i < apiRoutes.length; i++) {
+			// skip any methods in deny list for this uid
+			const method = apiRoutes[i].method;
+			const isDeniedMethod = _.get(settings, ['denyList', uid, method], false);
+			if (isDeniedMethod) {
+				continue;
+			}
+
 			// ensure path exists
 			if (!_.has(apiRoutes[i], 'config')) {
 				_.set(strapi, ['api', ct, 'routes', ct, 'routes', i, 'config'], {});
