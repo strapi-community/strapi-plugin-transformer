6cb34160be8ac61b0ba83069db176202922c7f86
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-02-05 20:49:16 -0500

    refactor(schema): add hook validation

--- server/config/schema.js
+++ server/config/schema.js
@@ -3,10 +3,23 @@
 const yup = require('yup');
 
 const pluginConfigSchema = yup.object().shape({
-	prefix: yup.string(),
 	responseTransforms: yup.object().shape({
 		removeAttributesKey: yup.bool(),
 		removeDataKey: yup.bool(),
+	}),
+	hooks: yup.object().shape({
+		preResponseTransform: yup.object().test({
+			name: 'preResponseTransform',
+			exclusive: true,
+			message: '${path} must be an object or function',
+			test: (value) => typeof value === 'function',
+		}),
+		postResponseTransform: yup.object().test({
+			name: 'postResponseTransform',
+			exclusive: true,
+			message: '${path} must be an object or function',
+			test: (value) => typeof value === 'function',
+		}),
 	}),
 });
 
